d88c4163d7d7ae8d9ac62f5cd7e7ed6c7be142d8
baihaiyin <baihaiyin@linkworld.cn>
2022-06-09 18:45:43 +0800

    add tbk sdk file

---
+++ src/top/request/TbkDgTpwdRiskReportRequest.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * TOP API: taobao.tbk.dg.tpwd.risk.report request
+ * 
+ * @author auto create
+ * @since 1.0, 2021.12.29
+ */
+class TbkDgTpwdRiskReportRequest
+{
+	/** 
+	 * 分页参数，一次最多不能超过1000
+	 **/
+	private $limit;
+	
+	/** 
+	 * 分页参数
+	 **/
+	private $offset;
+	
+	/** 
+	 * 如有pid，则查询pid下的relationid名单；如没有pid，则查询appkey关联userid下的pid名单
+	 **/
+	private $pid;
+	
+	private $apiParas = array();
+	
+	public function setLimit($limit)
+	{
+		$this->limit = $limit;
+		$this->apiParas["limit"] = $limit;
+	}
+
+	public function getLimit()
+	{
+		return $this->limit;
+	}
+
+	public function setOffset($offset)
+	{
+		$this->offset = $offset;
+		$this->apiParas["offset"] = $offset;
+	}
+
+	public function getOffset()
+	{
+		return $this->offset;
+	}
+
+	public function setPid($pid)
+	{
+		$this->pid = $pid;
+		$this->apiParas["pid"] = $pid;
+	}
+
+	public function getPid()
+	{
+		return $this->pid;
+	}
+
+	public function getApiMethodName()
+	{
+		return "taobao.tbk.dg.tpwd.risk.report";
+	}
+	
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+	
+	public function check()
+	{
+		
+		RequestCheckUtil::checkNotNull($this->limit,"limit");
+		RequestCheckUtil::checkNotNull($this->offset,"offset");
+	}
+	
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
---
+++ src/top/request/TbkDgVegasSendStatusRequest.php
@@ -0,0 +1,127 @@
+<?php
+/**
+ * TOP API: taobao.tbk.dg.vegas.send.status request
+ * 
+ * @author auto create
+ * @since 1.0, 2022.06.09
+ */
+class TbkDgVegasSendStatusRequest
+{
+	/** 
+	 * 入参类型(该模式下返回的结果为模糊匹配结果，和实际情况可能存在误差)： 1. IMEI 2. IDFA 3. OAID 4. MOBILE 5. ALIPAY_ID
+	 **/
+	private $deviceType;
+	
+	/** 
+	 * 加密后的值(ALIPAY_ID除外)，需用MD5加密，32位小写
+	 **/
+	private $deviceValue;
+	
+	/** 
+	 * 媒体pid
+	 **/
+	private $pid;
+	
+	/** 
+	 * 渠道管理id
+	 **/
+	private $relationId;
+	
+	/** 
+	 * 会员运营id
+	 **/
+	private $specialId;
+	
+	/** 
+	 * thor平台业务码， 1：coupon 超红
+	 **/
+	private $thorBizCode;
+	
+	private $apiParas = array();
+	
+	public function setDeviceType($deviceType)
+	{
+		$this->deviceType = $deviceType;
+		$this->apiParas["device_type"] = $deviceType;
+	}
+
+	public function getDeviceType()
+	{
+		return $this->deviceType;
+	}
+
+	public function setDeviceValue($deviceValue)
+	{
+		$this->deviceValue = $deviceValue;
+		$this->apiParas["device_value"] = $deviceValue;
+	}
+
+	public function getDeviceValue()
+	{
+		return $this->deviceValue;
+	}
+
+	public function setPid($pid)
+	{
+		$this->pid = $pid;
+		$this->apiParas["pid"] = $pid;
+	}
+
+	public function getPid()
+	{
+		return $this->pid;
+	}
+
+	public function setRelationId($relationId)
+	{
+		$this->relationId = $relationId;
+		$this->apiParas["relation_id"] = $relationId;
+	}
+
+	public function getRelationId()
+	{
+		return $this->relationId;
+	}
+
+	public function setSpecialId($specialId)
+	{
+		$this->specialId = $specialId;
+		$this->apiParas["special_id"] = $specialId;
+	}
+
+	public function getSpecialId()
+	{
+		return $this->specialId;
+	}
+
+	public function setThorBizCode($thorBizCode)
+	{
+		$this->thorBizCode = $thorBizCode;
+		$this->apiParas["thor_biz_code"] = $thorBizCode;
+	}
+
+	public function getThorBizCode()
+	{
+		return $this->thorBizCode;
+	}
+
+	public function getApiMethodName()
+	{
+		return "taobao.tbk.dg.vegas.send.status";
+	}
+	
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+	
+	public function check()
+	{
+		
+	}
+	
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
