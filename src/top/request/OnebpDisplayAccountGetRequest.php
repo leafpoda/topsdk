fea494930df378a283ebbb7195ae10e40cbfc6aa
henry <henry.liu@linkworld.cn>
2022-08-08 14:56:40 +0800

    sdk更新：账户余额api

---
+++ src/top/request/OnebpDisplayAccountGetRequest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * TOP API: taobao.onebp.display.account.get request
+ * 
+ * @author auto create
+ * @since 1.0, 2022.06.01
+ */
+class OnebpDisplayAccountGetRequest
+{
+	
+	private $apiParas = array();
+	
+	public function getApiMethodName()
+	{
+		return "taobao.onebp.display.account.get";
+	}
+	
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+	
+	public function check()
+	{
+		
+	}
+	
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
