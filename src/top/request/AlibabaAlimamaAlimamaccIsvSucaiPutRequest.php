891ec9aeec31fbe5b4d3ae6e0cd87c09950d5005
baihaiyin <baihaiyin@linkworld.cn>
2023-09-27 16:35:10 +0800

    add 阿里妈妈创意中心接口

---
+++ src/top/request/AlibabaAlimamaAlimamaccIsvSucaiPutRequest.php
@@ -0,0 +1,116 @@
+<?php
+/**
+ * TOP API: alibaba.alimama.alimamacc.isv.sucai.put request
+ * 
+ * @author auto create
+ * @since 1.0, 2023.06.02
+ */
+class AlibabaAlimamaAlimamaccIsvSucaiPutRequest
+{
+	/** 
+	 * 素材可访问地址
+	 **/
+	private $fileUrl;
+	
+	/** 
+	 * 素材高
+	 **/
+	private $height;
+	
+	/** 
+	 * 素材名称
+	 **/
+	private $name;
+	
+	/** 
+	 * 服务商系统中素材唯一标识
+	 **/
+	private $outerId;
+	
+	/** 
+	 * 素材宽
+	 **/
+	private $width;
+	
+	private $apiParas = array();
+	
+	public function setFileUrl($fileUrl)
+	{
+		$this->fileUrl = $fileUrl;
+		$this->apiParas["file_url"] = $fileUrl;
+	}
+
+	public function getFileUrl()
+	{
+		return $this->fileUrl;
+	}
+
+	public function setHeight($height)
+	{
+		$this->height = $height;
+		$this->apiParas["height"] = $height;
+	}
+
+	public function getHeight()
+	{
+		return $this->height;
+	}
+
+	public function setName($name)
+	{
+		$this->name = $name;
+		$this->apiParas["name"] = $name;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setOuterId($outerId)
+	{
+		$this->outerId = $outerId;
+		$this->apiParas["outer_id"] = $outerId;
+	}
+
+	public function getOuterId()
+	{
+		return $this->outerId;
+	}
+
+	public function setWidth($width)
+	{
+		$this->width = $width;
+		$this->apiParas["width"] = $width;
+	}
+
+	public function getWidth()
+	{
+		return $this->width;
+	}
+
+	public function getApiMethodName()
+	{
+		return "alibaba.alimama.alimamacc.isv.sucai.put";
+	}
+	
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+	
+	public function check()
+	{
+		
+		RequestCheckUtil::checkNotNull($this->fileUrl,"fileUrl");
+		RequestCheckUtil::checkNotNull($this->height,"height");
+		RequestCheckUtil::checkNotNull($this->name,"name");
+		RequestCheckUtil::checkNotNull($this->outerId,"outerId");
+		RequestCheckUtil::checkNotNull($this->width,"width");
+	}
+	
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
---
+++ src/top/request/AlibabaAlimamaAlimamaccIsvSucaiQueryRequest.php
@@ -0,0 +1,114 @@
+<?php
+/**
+ * TOP API: alibaba.alimama.alimamacc.isv.sucai.query request
+ * 
+ * @author auto create
+ * @since 1.0, 2023.07.12
+ */
+class AlibabaAlimamaAlimamaccIsvSucaiQueryRequest
+{
+	/** 
+	 * 素材名称
+	 **/
+	private $name;
+	
+	/** 
+	 * 分页
+	 **/
+	private $page;
+	
+	/** 
+	 * 分页个数
+	 **/
+	private $perPage;
+	
+	/** 
+	 * 尺寸，x 分隔
+	 **/
+	private $size;
+	
+	/** 
+	 * 素材类型
+	 **/
+	private $type;
+	
+	private $apiParas = array();
+	
+	public function setName($name)
+	{
+		$this->name = $name;
+		$this->apiParas["name"] = $name;
+	}
+
+	public function getName()
+	{
+		return $this->name;
+	}
+
+	public function setPage($page)
+	{
+		$this->page = $page;
+		$this->apiParas["page"] = $page;
+	}
+
+	public function getPage()
+	{
+		return $this->page;
+	}
+
+	public function setPerPage($perPage)
+	{
+		$this->perPage = $perPage;
+		$this->apiParas["per_page"] = $perPage;
+	}
+
+	public function getPerPage()
+	{
+		return $this->perPage;
+	}
+
+	public function setSize($size)
+	{
+		$this->size = $size;
+		$this->apiParas["size"] = $size;
+	}
+
+	public function getSize()
+	{
+		return $this->size;
+	}
+
+	public function setType($type)
+	{
+		$this->type = $type;
+		$this->apiParas["type"] = $type;
+	}
+
+	public function getType()
+	{
+		return $this->type;
+	}
+
+	public function getApiMethodName()
+	{
+		return "alibaba.alimama.alimamacc.isv.sucai.query";
+	}
+	
+	public function getApiParas()
+	{
+		return $this->apiParas;
+	}
+	
+	public function check()
+	{
+		
+		RequestCheckUtil::checkNotNull($this->page,"page");
+		RequestCheckUtil::checkNotNull($this->perPage,"perPage");
+		RequestCheckUtil::checkNotNull($this->type,"type");
+	}
+	
+	public function putOtherTextParam($key, $value) {
+		$this->apiParas[$key] = $value;
+		$this->$key = $value;
+	}
+}
